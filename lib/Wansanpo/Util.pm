f45d4b9c0cd0aca17e3030e0a58b696bc6c47717
yk <yosizuka1@gmail.com>
2017-10-18 13:56:52 +0900

    add icon

--- lib/Wansanpo/Util.pm
+++ lib/Wansanpo/Util.pm
@@ -4,6 +4,7 @@ use Time::Piece;
 use Exporter 'import';
 our @EXPORT_OK = qw{
     now_datetime
+    easy_filename
 };
 
 =encoding utf8
@@ -19,6 +20,20 @@ sub now_datetime {
     my $date = $t->date;
     my $time = $t->time;
     return "$date $time";
+}
+
+# 簡易的にユニークなファイル名作成
+sub easy_filename {
+
+    # 日付情報
+    my $t        = localtime;
+    my $datetime = $t->strftime('%Y%m%d%H%M%S');
+
+    # 4桁の簡易的な乱数
+    my $rand     = int rand(1000);
+    my $id       = sprintf '%04d', $rand;
+    my $filename = $datetime . '_' . $id;
+    return $filename;
 }
 
 1;
